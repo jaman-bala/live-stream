8377254bdfab3df7aa1ff57c3e92c2a423d2a333
jaman-bala <ermekoffdastan@yandex.ru>
2023-12-06 18:47:23 +0600

    add async

--- stream/stream/asgi.py
+++ stream/stream/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for stream project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
+from django.conf import settings
+from uvicorn.workers import UvicornWorker
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stream.settings')
 
 application = get_asgi_application()
+
+# Добавьте следующие строки для асинхронного запуска через uvicorn
+if settings.DEBUG:
+    from channels.layers import get_channel_layer
+    channel_layer = get_channel_layer()
+    import django
+
+    django.setup()
+
+
+def get_asgi_application():
+    return application
--- stream/video/views.py
+++ stream/video/views.py
@@ -1,22 +1,43 @@
-from ninja import Router
-import time
-from django.http import StreamingHttpResponse
+import asyncio
+import httpx
 import cv2
+from ninja import Router
+from django.http import StreamingHttpResponse
 from asgiref.sync import sync_to_async
 from typing import List
-
 from starlette.responses import JSONResponse
 
 from .models import Stream
 from api.schemas import StreamSchema
 
 app_router = Router()
+cv2.ocl.setUseOpenCL(True)
 
 
 @app_router.get("/streams/", response=List[StreamSchema])
-def get_streams(request):
-    qs = Stream.objects.all()
+async def get_streams(request):
+    qs = await sync_to_async(list)(Stream.objects.all())
     return qs
+
+
+async def async_cap_read(cap):
+    return await sync_to_async(cap.read)()
+
+
+async def async_imencode(frame):
+    return await sync_to_async(cv2.imencode)('.jpg', frame)
+
+
+async def gen_frames(rtsp_url):
+    cap = cv2.VideoCapture(rtsp_url)
+    while True:
+        ret, frame = await async_cap_read(cap)
+        if not ret:
+            break
+        _, buffer = await async_imencode(frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        await asyncio.sleep(0.01)
 
 
 @app_router.get("/streams/{stream_id}")
@@ -24,18 +45,6 @@ async def stream_view(request, stream_id: int):
     try:
         stream = await sync_to_async(Stream.objects.get)(id=stream_id)
         return StreamingHttpResponse(await sync_to_async(gen_frames)(stream.rtsp_url),
-                                 content_type='multipart/x-mixed-replace; boundary=frame')
+                                     content_type='multipart/x-mixed-replace; boundary=frame')
     except Stream.DoesNotExist:
         return JSONResponse(content={"error": "Stream not found"}, status_code=404)
-
-
-def gen_frames(rtsp_url):
-    cap = cv2.VideoCapture(rtsp_url)
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        _, buffer = cv2.imencode('.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-        time.sleep(0.1)
